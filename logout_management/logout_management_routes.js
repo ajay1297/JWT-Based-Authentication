6190a24f8ea1fb5be3aab9bd839ed57edded527a
Ajay Police Patil <37379256+ajay1297@users.noreply.github.com>
2021-08-29 16:47:25 +0530

    Delete logout_management directory

--- logout_management/logout_management_routes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const LoginManagement = require('./logout_management');
-
-router.post('/', LoginManagement.logout);
-
-module.exports = router;
